4c2d0597e5491a2a14f0c806698dae587be1d09a
jbirky <jbirky@uw.edu>
2024-08-06 13:36:41 -0700

    bounds

--- jbirky/run_alabi.py
+++ jbirky/run_alabi.py
@@ -22,18 +22,18 @@ from johnstone_model import StellarEvolutionModel
 # Prot_data = np.array([3.295, 0.003]) * u.day
 # age_data = np.array([7.6, 2.2]) * u.Gyr
 
-# star_name = "GJ 3470"
-# mass_data = np.array([0.51, 0.06]) * u.Msun
-# Lbol_data = np.array([0.029, 0.002]) * u.Lsun
-# Lxray_data = np.array([4.43e27, 7.88e26]) * u.erg/u.s
-# Prot_data = np.array([21.54, 0.49]) * u.day
+star_name = "GJ_3470"
+mass_data = np.array([0.51, 0.06]) * u.Msun
+Lbol_data = np.array([0.029, 0.002]) * u.Lsun
+Lxray_data = np.array([4.43e27, 7.88e26]) * u.erg/u.s
+Prot_data = np.array([21.54, 0.49]) * u.day
 
-star_name = "55_Cnc"
-mass_data = np.array([0.85, 0.02]) * u.Msun
-Lbol_data = np.array([0.582, 0.014]) * u.Lsun
-Lxray_data = np.array([6.05e26, 5.23e25]) * u.erg / u.s
-Prot_data = np.array([38.8, 0.05]) * u.day
-age_data = np.array([10.2, 2.5]) * u.Gyr
+# star_name = "55_Cnc"
+# mass_data = np.array([0.85, 0.02]) * u.Msun
+# Lbol_data = np.array([0.582, 0.014]) * u.Lsun
+# Lxray_data = np.array([6.05e26, 5.23e25]) * u.erg / u.s
+# Prot_data = np.array([38.8, 0.05]) * u.day
+# age_data = np.array([10.2, 2.5]) * u.Gyr
 
 # ========================================================
 # Configure the model
@@ -115,20 +115,21 @@ def lnlike(theta):
     _ = model.LXUV_model(theta)
     chi2_array = model.compute_chi_squared_fit()
     lnl = -0.5 * np.sum(chi2_array)
+    print("lnlike: ", lnl)
     return lnl
 
-def lnpost(theta):
-    return lnlike(theta) + lnprior(theta)
+# def lnpost(theta):
+#     return lnlike(theta) + lnprior(theta)
 
 # ========================================================
 # Run alabi
 # ========================================================
 
 # change these to run different models
-test = "model2"
+test = "model1"
 model = eval(test)
 
-save_dir = f"results/{star_name}/{test}"
+save_dir = f"results/{star_name}/{test}/"
 
 kernel = "ExpSquaredKernel"
 
@@ -144,9 +145,9 @@ labels = [r"$m_{\star}$ [M$_{\odot}$]",
 if __name__ == "__main__":
 
     # Initialize the surrogate model
-    sm = SurrogateModel(fn=lnpost, bounds=bounds, prior_sampler=ps, 
+    sm = SurrogateModel(fn=lnlike, bounds=bounds, prior_sampler=ps, 
                         savedir=save_dir, cache=True,
-                        labels=labels, scale=None, ncore=20)
+                        labels=labels, scale=None, ncore=22)
 
     # Compute an initial training sample and train the GP
     sm.init_samples(ntrain=200, ntest=100, reload=False)
@@ -156,59 +157,17 @@ if __name__ == "__main__":
     sm.active_train(niter=1000, algorithm="bape", gp_opt_freq=10)
     sm.plot(plots=["gp_all"])
 
-    # Reload the saved model and run MCMC
-    sm = alabi.cache_utils.load_model_cache(save_dir)
-    sm.savedir = save_dir
+    # # Reload the saved model and run MCMC
+    # sm = alabi.cache_utils.load_model_cache(save_dir)
+    # sm.savedir = save_dir
+
+    # sm.active_train(niter=500, algorithm="bape", gp_opt_freq=10)
+    # sm.plot(plots=["gp_all"])
 
     # MCMC with emcee
-    sm.run_emcee(lnprior=lnprior, nwalkers=50, nsteps=int(5e4), opt_init=False)
+    sm.run_emcee(lnprior=lnprior, nwalkers=50, nsteps=int(1e6), opt_init=False)
     sm.plot(plots=["emcee_corner"])
 
     # MCMC with dynesty
     sm.run_dynesty(ptform=prior_transform, mode='dynamic')
     sm.plot(plots=["dynesty_all"])
-
-
-    # ========================================================
-    # Corner plot with priors 
-
-    import corner 
-    from scipy.stats import norm
-
-    emcee_samples = np.load(f"{save_dir}/emcee_samples_final.npz")["samples"]
-    dynesty_samples = np.load(f"{save_dir}/dynesty_samples_final.npz")["samples"]
-
-    lw = 1.5
-    colors = ["dimgrey", "royalblue", "r"]
-
-    fig = corner.corner(emcee_samples,  labels=labels, range=bounds,
-                        show_titles=True, verbose=False, max_n_ticks=4,
-                        plot_contours=True, plot_datapoints=True, plot_density=True,
-                        color=colors[0], no_fill_contours=False, title_kwargs={"fontsize": 16},
-                        label_kwargs={"fontsize": 22}, hist_kwargs={"linewidth":2.0, "density":True})
-    
-    fig = corner.corner(dynesty_samples, labels=labels, range=bounds, 
-                        show_titles=True, verbose=False, max_n_ticks=4, title_fmt='.3f',
-                        plot_contours=True, plot_datapoints=True, plot_density=True,
-                        color=colors[1], no_fill_contours=False, title_kwargs={"fontsize": 16},
-                        label_kwargs={"fontsize": 22}, hist_kwargs={"linewidth":2.0, "density":True},
-                        fig=fig)
-    
-    ax_list = fig.axes
-
-    xtext = 3.5
-    fig.axes[1].text(xtext, 0.725, r"--- Literature Priors", fontsize=26, color=colors[2], ha='left')
-    fig.axes[1].text(xtext, 0.55, r"--- emcee Posterior", fontsize=26, color=colors[0], ha='left')
-    fig.axes[1].text(xtext, 0.375, r"--- dynesty Posterior", fontsize=26, color=colors[1], ha='left')
-
-    panel = 0
-    for ii in range(len(bounds)):
-        x = np.linspace(bounds[ii][0], bounds[ii][1], 100)
-        if prior_data[ii][0] is not None:
-            ax_list[panel].plot(x, norm.pdf(x, loc=prior_data[ii][0], scale=prior_data[ii][1]),
-                                lw=lw, color=colors[2], linestyle='--')
-        else:
-            ax_list[panel].axhline(1 / (bounds[ii][1] - bounds[ii][0]), lw=lw, color=colors[2], linestyle='--')
-        panel += len(bounds) + 1
-
-    fig.savefig(f"{save_dir}/corner_plot_with_priors.png", dpi=300, bbox_inches="tight")
